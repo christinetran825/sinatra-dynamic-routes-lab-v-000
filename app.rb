3bdedeaaf11b1b3c075bbf01d036925546c2121c
Christine Tran <christinetran825@gmail.com>
2017-10-13 20:24:15 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -14,7 +14,8 @@ class App < Sinatra::Base
   end
 
   get '/say/:number/:phrase' do
-
+    @message = params[:number].times do params[:phrase] end
+    "#{@message}"
   end
 
   get '/say/:word1/:word2/:word3/:word4/:word5' do
