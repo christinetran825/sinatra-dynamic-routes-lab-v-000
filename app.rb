10b96057e596acd6c6dd9416d15769b2a4835968
Christine Tran <christinetran825@gmail.com>
2017-10-13 20:20:22 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -11,11 +11,11 @@ class App < Sinatra::Base
 
   end
 
-  get '/square/:number' do
+  get '/say/:number/:phrase' do
 
   end
 
-  get '/square/:number' do
+  get '/say/:word1/:word2/:word3/:word4/:word5' do
 
   end
 
