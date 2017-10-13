28d49ef061878e95c358bec1fe71f52e6b59218f
Christine Tran <christinetran825@gmail.com>
2017-10-13 20:35:41 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -20,8 +20,9 @@ class App < Sinatra::Base
   end
 
   get '/say/:word1/:word2/:word3/:word4/:word5' do
-    @message = params[:word1]+params[:word2]+params[:word3]+params[:word4]+params[:word5]
-    "#{@message.join(" ")}."
+    message = params[:word1]+params[:word2]+params[:word3]+params[:word4]+params[:word5]
+    @new_message = message.join(" ")
+    "#{@message}."
   end
 
   get '/:operation/:number1/:number2' do
