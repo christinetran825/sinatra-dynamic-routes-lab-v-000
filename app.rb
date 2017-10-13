8f498c98744fc2fc2d0e61127b7f6874a0789e79
Christine Tran <christinetran825@gmail.com>
2017-10-13 20:43:17 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -29,12 +29,12 @@ class App < Sinatra::Base
   end
 
   get '/:operation/:number1/:number2' do
-    @number1 = params[:number1]
-    @number2 = params[:number2]
+    number1 = params[:number1].to_i
+    number2 = params[:number2].to_i
 
     case params[:operation]
     when 'add'
-      "#{@number1.to_i} #{@operation} #{@number1.to_i}"
+      "#{@number1 + @number1.to_i}"
     when 'subtract'
       "#{@number1.to_i} #{@operation} #{@number1.to_i}"
     when 'multiply'
